d3a4f93650311388d65844149a88fc4b293e132a
julienhouyet <julien.houyet@gmail.com>
2024-07-15 10:03:51 +0200

    feat: Add easyfind function for finding values in a container

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,7 +6,26 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:41:06 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/07/10 16:41:07 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/07/15 10:02:33 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iterator>
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+typename T::iterator easyfind(T &array, int value)
+{
+	typename T::iterator it = std::find(array.begin(), array.end(), value);
+	if (it == array.end())
+	{
+		throw std::runtime_error("Value not found");
+	}
+	return it;
+}
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,15 +6,41 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:41:02 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/07/10 16:42:47 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/07/15 10:03:23 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
 #include <iostream>
+#include <vector>
 
 int main()
 {
-	std::cout << "Hello Easy Find" << std::endl;
+	int values[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	std::vector<int> array;
+
+	array.assign(values, values + sizeof(values) / sizeof(values[0]));
+
+	try
+	{
+		std::vector<int>::iterator it = easyfind(array, 9);
+		std::cout << *it << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		std::vector<int>::iterator it = easyfind(array, 20);
+		std::cout << *it << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	return 0;
 }
