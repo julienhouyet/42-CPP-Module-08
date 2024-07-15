19a5d88d903c03e886ca6f1e47c7174bef1dcc88
julienhouyet <julien.houyet@gmail.com>
2024-07-15 10:08:36 +0200

    feat: ex01/span files and Makefile

---
+++ ex01/src/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 10:05:11 by jhouyet           #+#    #+#             */
+/*   Updated: 2024/07/15 10:08:17 by jhouyet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "Hello Span" << std::endl;
+
+	// Span sp = Span(5);
+
+	// sp.addNumber(6);
+	// sp.addNumber(3);
+	// sp.addNumber(17);
+	// sp.addNumber(9);
+	// sp.addNumber(11);
+
+	// std::cout << sp.shortestSpan() << std::endl;
+
+	// std::cout << sp.longestSpan() << std::endl;
+
+	// return;
+}
