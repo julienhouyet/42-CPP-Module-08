1d86f1e3d3324c0e1207d72156fe52de5fd6c6f7
julienhouyet <julien.houyet@gmail.com>
2024-07-18 17:02:13 +0200

    chore: update main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 16:59:01 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/07/18 17:00:40 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/07/18 17:01:54 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,27 @@
 int main()
 {
 	std::cout << "Hello MutanStack" << std::endl;
+
+	// MutantStack<int> mstack;
+	// mstack.push(5);
+	// mstack.push(17);
+	// std::cout << mstack.top() << std::endl;
+	// mstack.pop();
+	// std::cout << mstack.size() << std::endl;
+	// mstack.push(3);
+	// mstack.push(5);
+	// mstack.push(737);
+	// //[...]
+	// mstack.push(0);
+	// MutantStack<int>::iterator it = mstack.begin();
+	// MutantStack<int>::iterator ite = mstack.end();
+	// ++it;
+	// --it;
+	// while (it != ite)
+	// {
+	// 	std::cout << *it << std::endl;
+	// 	++it;
+	// }
+	// std::stack<int> s(mstack);
+	// return 0;
 }
